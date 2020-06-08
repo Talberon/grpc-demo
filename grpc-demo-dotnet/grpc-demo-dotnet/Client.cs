c7e680ac428e429c8785b01c32857c36db68d6c5
Talberon <atheion@outlook.com>
2020-06-08 20:52:30 -0300

    Added simple dotnet client

---
+++ grpc-demo-dotnet/grpc-demo-dotnet/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Grpc.Core;
+using Moviecatalog;
+
+namespace grpc_demo_dotnet
+{
+    internal static class Client
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Starting gRPC client...");
+
+            var channel = new Channel("localhost", 8080, ChannelCredentials.Insecure);
+            var client = new MovieCatalog.MovieCatalogClient(channel);
+
+            var fetchRequest = new FetchMovieRequest
+            {
+                Name = "War Games"
+            };
+
+            MovieItem response = client.FetchExistingMovie(fetchRequest);
+            Console.WriteLine($"Fetched movie from server: {response}");
+
+            var createRequest = new MovieItem
+            {
+                Name = "Toy Story",
+                Price = 29.99d,
+                InStock = false
+            };
+
+            AddMovieResponse saveResponse = client.SaveNewMovie(createRequest);
+            Console.WriteLine($"Saved new movie: {saveResponse}");
+        }
+    }
+}
